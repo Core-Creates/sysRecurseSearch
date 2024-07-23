7f5c76811ca5624f6b4d3c6ae60c38cc5c212fd2
Core Alcoser <corrinaalcoser@gmail.com>
2024-07-23 04:43:20 -0500

    adda __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 23:12:49 2024
+
+@author: Corrina Alcoser
+github: core-creates
+library: file_search/__init__.py
+info: recursively search through all directories on all drives on the system
+"""
+
+# __init__.py
+from .search import list_files, find_file, get_drives, list_directories, last_modified_file
