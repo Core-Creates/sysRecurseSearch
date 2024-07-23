14090d60eb5f56e47a5283221320c5b778ce8372
Core Alcoser <corrinaalcoser@gmail.com>
2024-07-23 04:39:12 -0500

    Add file_search.py

---
+++ file_search.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 22:48:41 2024
+
+@author: Corrina Alcoser
+github: core-creates
+library: file_search 
+info: recursively search through all directories on all drives on the system
+
+"""
+
+
