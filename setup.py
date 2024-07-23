9a4d4d90aec5ad4394b4bd0e9d40e53f46172b92
Core Alcoser <corrinaalcoser@gmail.com>
2024-07-23 04:52:02 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sysRecurseSearch',
+    packages=find_packages(include=['sysRecurseSearch']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Corrina Alcoser aka Core-Creates',
+)
